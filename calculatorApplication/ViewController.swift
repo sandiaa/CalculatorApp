44ac5102a7998f3eb2df782ef99fc285c937f785
Admin <admin@admins-macbook-pro-2.local>
2018-12-21 12:08:05 +0530

    Initial Commit

---
+++ calculatorApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculatorApplication
+//
+//  Created by Admin on 21/12/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
